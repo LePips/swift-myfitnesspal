0fc50da5b476b3efdc290c3e826435dd92f84738
Ethan Pippin <ethanpippin2343@gmail.com>
2021-06-12 00:41:49 -0600

    Make sample day

--- Sources/MyFitnessPal/Types/Day.swift
+++ Sources/MyFitnessPal/Types/Day.swift
@@ -14,4 +14,18 @@ public struct Day {
     public let goalMacros: Macros
     public let remainingMacros: Macros
     public let exerciseCalories: Int
+    
+    public var sample: Day {
+        
+        let total = Macros(calories: 1234, carbs: 93, fat: 19, protein: 102, sodium: 1200, sugar: 33)
+        let goal = Macros(calories: 2100, carbs: 200, fat: 40, protein: 180, sodium: 2300, sugar: 92)
+        let remaining = Macros(calories: goal.calories - total.calories,
+                               carbs: goal.carbs - total.carbs,
+                               fat: goal.fat - total.fat,
+                               protein: goal.protein - total.protein,
+                               sodium: goal.sodium - total.sodium,
+                               sugar: goal.sugar - total.sugar)
+        
+        return Day(meals: [], totalMacros: total, goalMacros: goal, remainingMacros: remaining, exerciseCalories: 11)
+    }
 }
